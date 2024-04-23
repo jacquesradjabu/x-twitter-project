644ae210de550196bab2acb121ef50bf3a0fb3e4
jacquesradjabu <143694287+jacquesradjabu@users.noreply.github.com>
2024-04-23 11:18:36 +0100

    mise en jour

--- src/pages/profile/screens/Posts.jsx
+++ src/pages/profile/screens/Posts.jsx
@@ -1,20 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { twitterCurrentUserInDB } from "../../../data/ImagesIcons";
 import Tweet from '../../../components/tweets/Tweet';
 
 const Posts = () => {
+
+   const [db, setDB] = useState(twitterCurrentUserInDB);
+   const [isLoading, setIsLoading] = useState(true);
+   const [message, setMessage] = useState("Loading...");
+
+   useEffect(function () {
+      setTimeout(function () {
+         fetch('https://66260ec3052332d55321759f.mockapi.io/api/posts/current-user')
+            .then(reponse => reponse.json())
+            .then(data => { 
+               setDB(data);
+               setIsLoading(false)
+            })
+            .catch(err => setMessage('erreur de conexion au serveur'))
+      }, 1000)  
+   }, [])
+
+   
+
+
+
+
+
    return (
       <>
          {
-            twitterCurrentUserInDB.map((tweet, i) => (
-               <Tweet 
-                  key={i.toString('2')}
-                  tweetsDB={tweet}
-               />
-            ))
+            isLoading ? <h1 className="text-3xl mt-12 text-center text-blue-500">{message}</h1> : (
+               db
+                  .filter(t => t.nickname == '@bradley')
+                  .map((tweet, i) => (
+                  <Tweet
+                     key={i.toString('2')}
+                     tweetsDB={tweet}
+                  />
+               ))
+            )
          }
       </>
    );
 };
 
-export default Posts;
+export default Posts; 
--- src/providers/TweetPostProvider.jsx
+++ src/providers/TweetPostProvider.jsx
@@ -1,12 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { TweetPostContext } from "../contexts/TweetPostContext";
 import { twitterCurrentUserInDB } from "../data/ImagesIcons";
 
-export const TweetPostProvider = ({children}) => {
+
+export const TweetPostProvider = ({ children }) => {
+
    const [updatePost, setUpdatePost] = useState(twitterCurrentUserInDB);
-   return (
-      <TweetPostContext.Provider value={{ updatePost, setUpdatePost }}>
-         {children}
-      </TweetPostContext.Provider>
-   );
+   const [isLoading, setIsLoading] = useState(true);
+   const [mesage, setMessage] = useState("Loading...");
+  
+   useEffect(() => {
+      setTimeout(() => {
+         fetch('https://66260ec3052332d55321759f.mockapi.io/api/posts/current-user') 
+            .then(response => response.json())
+            .then(data => {
+               setUpdatePost(data);
+               setIsLoading(false);
+            })
+            .catch(err => setMessage('Erreur de connexion avec le serveur'))
+      }, 1000)
+   }, [])
+
+return (
+   <TweetPostContext.Provider value={{ updatePost, setUpdatePost }}>
+      {isLoading ? <h1 className="text-3xl mt-12 text-center text-blue-500">{mesage}</h1> : children}
+   </TweetPostContext.Provider>
+);
 };
+
